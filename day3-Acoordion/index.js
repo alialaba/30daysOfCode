c1e6815f975019baebf42eca4d42db2347cb26cd
alialaba <alialaba079@gmail.com>
2023-05-10 13:59:13 -0400

    ft: toggle accordion

---
+++ day3-Acoordion/index.js
@@ -0,0 +1,9 @@
+const accordionItem = document.getElementsByClassName("accordion-item");
+
+
+//Loop through all the accordion item 
+for(let i = 0; i < accordionItem.length; i++){
+    accordionItem[i].addEventListener("click", function(){
+        this.classList.toggle("active")
+    })
+}
